fec76dec6ed502dd01dec30337338613a5bde6d5
WISS-Student <nadine.duc@wiss-edu.ch>
2026-06-06 15:39:11 +0200

    Fixed localhost redirect

--- M223_Frontend/src/contexts/AuthContext.jsx
+++ M223_Frontend/src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState, useEffect } from 'react'
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8080/api/auth'
+const API_URL = (import.meta.env.VITE_API_URL || 'http://localhost:8080/api') + "/auth"
 
 const AuthContext = createContext()
 
